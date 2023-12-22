10974ff8ab0c7ba93a842d0d1411df462d0aabf8
rusfirefly <fesov_86@mail.ru>
2023-12-22 07:54:48 +0300

    practic_1_4

---
+++ practic_1_4/practic_1_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace practic_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+            //задание 2
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadKey();
+        }
+    }
+}
